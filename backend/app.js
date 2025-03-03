f3d8dbb2bcd81d85e19ada352a3330aff9d6ca2a
Kashinath G. Meshram <75173919+Kartik-yo@users.noreply.github.com>
2025-03-04 01:32:56 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Kartik! Your DevOps journey starts here 🚀");
+  res.send("Hello World");
 });
 
-app.get("/health", (req, res) => {
-  res.json({ status: "UP" });
-});
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app; // Export app
